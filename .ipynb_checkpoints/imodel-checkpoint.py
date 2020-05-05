89ab59cdfe85b55d261630978993b700441226a8
medikid <soma.programmer@gmail.com>
2020-05-05 21:56:02 +0000

    fixed imodel

---
+++ .ipynb_checkpoints/imodel-checkpoint.py
@@ -0,0 +1,151 @@
+
+import os
+#!python3 '../ifile.py' 
+# from ipynb.fs.full.ifile import iFile
+# from ipynb.fs.full.idata import Data
+# from ipynb.fs.full.idataset import Dataset
+from ifile import iFile
+from idataset import Dataset
+
+class Model(iFile):
+    _M=None;
+    _ID=None;
+    _GAME=None;
+    _API = None;
+    _BUILD = None; #model architecture/layers
+    _MAKE = 0; #Same build, but differnet make parameters
+    _VERSION = None; #training / epoch 
+    
+    #set default model
+    _FOLDER_TYPE = 'models'
+    _FOLDER_PATH = None;
+    _FILE_FORMAT = None
+    
+    
+    _IS_CHECKPOINT = False;
+    _CHECKPOINTS_FOLDER=None;
+    
+    def __init__(self, ModelID, Dataset, FileFormat='h5'):
+        self._ID = ModelID;
+        super().__init__(ModelID, Dataset._FOLDER_PATH, FileFormat);
+        self.decipher_file_name();
+        
+        #we are not deriving folder_path, but using dataset folder
+                
+        self.derive_file_path();        
+        self.derive_game_path(Dataset._INFO) #derives 'data/keno'
+        self.derive_full_path();
+        self.derive_checkpoints_folder();
+        print("[iModel:__init__")
+        
+    def set_decipher_info(self):
+        self._INFO['GAME'] = self._GAME;
+        self._INFO['API'] = self._API;
+        self._INFO['BUILD'] = self._BUILD;
+        self._INFO['VERSION'] = self._VERSION;
+        print("[iModel:set_decipher_info] {0}".format(self._INFO))
+        
+    def decipher_file_name(self, Delimiter="."):        
+        ids = self._FILE_NAME.split(Delimiter);
+        try:
+            self._GAME = ids[0]
+            self._API = ids[1]
+            self._BUILD = ids[2]
+            self._VERSION = ids[3]
+        except IndexError:
+            self.LoadBestVersion();
+        
+        print("[iModel:decipher_file_name]")
+        
+        self.set_decipher_info();        
+    
+               
+    def derive_file_path(self):
+        #derive file name from decipher  
+        #FilePath = self._FOLDER_PATH;
+        FilePath = self._FOLDER_TYPE + "/";
+        FilePath += self._INFO['API'] + "/";
+        FilePath += self._INFO['BUILD'] + "/";
+        #FilePath += "/" + self._INFO['VERSION'];
+        FilePath += self._FILE_NAME;
+        FilePath += "." + self._FILE_FORMAT;
+        
+        self._FILE_PATH = FilePath;
+        print("[iModel:derive_file_path] {0}".format(self._FILE_PATH))
+        
+    def derive_checkpoints_folder(self):
+        self._CHECKPOINTS_FOLDER = os.path.dirname(self._FULL_PATH) + '/checkpoints';
+        self.ensure_dirs(self._CHECKPOINTS_FOLDER)
+        
+    
+    def load(self):
+        print("[iModel:load] Not Implemented")
+        pass;
+    
+    def get_untrained_folder_path(self):
+        return self._ROOT_FOLDER + self._GAME_PATH +'untrained_models/';
+        
+ 
+    def load_untrained(self):
+        untrained_file_path = self.get_untrained_folder_path() + self._GAME + "." + self._API + "." + self._BUILD + ".h5";
+        self._M.load(untrained_file_path);
+        print("Saved untrained model {0}".format(untrained_file_path));
+       
+    def save_untrained(self):
+        untrained_file_path = self.get_untrained_folder_path()  + self._GAME + "." + self._API + "." + self._BUILD + ".h5";
+        self._M.save(untrained_file_path);
+        print("Saved untrained model {0}".format(untrained_file_path));
+        
+    def get_max_checkpoint(self):
+        _MAX_CHKPNT = 0;
+        for f in os.listdir(self._CHECKPOINTS_FOLDER):
+            chkpnt = int(f.split('.')[-2][1:])
+            if (chkpnt > max_chkpnt):
+                _MAX_CHKPNT = chkpnt;
+        print("Latest checkpoint is {0}".format(max_chkpnt))
+        return _MAX_CHKPNT;
+        
+    def load_latest_checkpoint(self):
+        _MAX_CHKPNT = self.get_max_checkpoint();
+        self._VERSION='e' + str(_MAX_CHKPNT).zfill(4);
+        self._INFO['VERSION'] = self._VERSION;
+        MaxFileName = self._GAME + '.' + self._API + '.' + self._BUILD + '.' + self._MAKE + '.' + self._VERSION + '.h5';
+        self._FILE_NAME = MaxFileName;
+        self.load_checkpoint();
+        
+    def load_checkpoint(self):
+        chkpnt_file_path = self._CHECKPOINTS_FOLDER + '/' + self._FILE_NAME;
+        self._M.load(chkpnt_file_path);
+        print('[iModel:load_checkpoint] Loaded {0}'.format(chkpnt_file_path))
+        
+    def save_checkpoint(self, FileName):
+        chkpnt_file_path = self._CHECKPOINTS_FOLDER + '/' + FileName;
+        print('[iModel:save_checkpoint] Saved {0}'.format(chkpnt_file_path))
+    
+    def load_best_version(self):
+        print("[iModel:load_best_version] Not Implemented")
+        pass;
+    
+    def load_latest_version(self):
+        print("[imodel: load_latest_version] Loading")
+        self.load_latest_checkpoint();
+        pass;
+           
+             
+    def get_model_summary(self):
+        return self._M.summary();
+    
+    def train(self, x, y, Epochs=10, BatchSize=32):
+        self._M.fit(x, y, epochs=Epochs, batch_size=BatchSize);        
+        pass;
+    
+    def predict(self, x_test):
+        return y_hat;
+    
+    def save(self):
+        self._M.save(self._FULL_PATH)
+        
+        
+    def print_summary(self):
+        print(self._M.summary())
+                
---
+++ .ipynb_checkpoints/ker_model-checkpoint.py
@@ -0,0 +1,66 @@
+
+#!python3 '../imodel.py' #from ipynb.fs.full.imodel 
+from imodel import Model
+from ker_model_loader import KER_Model_Loader
+
+import keras as kr
+from keras.models import Sequential, load_model
+from keras.layers import LSTM, Dense
+
+import numpy as np
+import pandas as pd
+
+class KER_Model(Model):
+    _MODEL_PATH = None;
+    _CHECKPOINTS=[];
+    
+    def __init__(self, ModelID, Dataset, FileFormat='h5'):
+        super().__init__(ModelID, Dataset, FileFormat)
+        self._MODEL_PATH = self._FULL_PATH;
+        print("[KER_Model:__init__]")
+ 
+    def load(self, Untrained=False):
+        print("[KER_Model:load]: Loading {0}".format(self._FULL_PATH))
+        if(Untrained ==  True):
+            self.load_untrained();
+        elif (self._IS_CHECKPOINT == True):
+            self.load_checkpoint();
+        else:
+            KER_Model_Loader(self); #actually creates new model
+        
+        #compile once again
+        #self._M.compile(optimizer='adam', loss='mse', metrics=['accuracy']);
+        
+        #print("[KER_Model:load]: Loaded {0}".format(self._M.summary()))
+        pass;
+    
+    def load_checkpoint(self):    
+        chkpnt_file_path = self._CHECKPOINTS_FOLDER +  self._FILE_NAME +'.' + self._FILE_FORMAT;
+        self._M = load_model(chkpnt_file_path);
+        print('[KER_Model:load_checkpoint] Loaded {0}'.format(chkpnt_file_path))
+    
+    def load_untrained(self):
+        untrained_file_path = self.get_untrained_folder_path() + self._GAME + "." + self._API + "." + self._BUILD + ".h5";
+        self._M = load_model(untrained_file_path);
+        print("[KER_Model:load_untrained] {0}".format(untrained_file_path));
+        
+    def save(self):
+        self._M.save(self._FULL_PATH)
+        print("[KER_Model:save]: Saving {0}".format(self._FULL_PATH))
+    
+    def load_best_version(self):
+        pass;
+    
+    def load_latest_version(self):
+        pass;
+    
+    def predict(self, X_test, isBatch=True):
+        Y_hat = np.array()
+        if(isBatch == True):
+            Y_hat = self._M.predict_on_batch(X_test);
+        else:
+            Y_hat = self._M.predict(X_test);
+            
+        return Y_hat;     
+    
+    
