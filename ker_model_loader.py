7163f4a0a7a5595f5be87a0b0948a1c76c042e76
medikid <soma.programmer@gmail.com>
2020-05-20 09:34:53 +0000

    added ker 4.4 and 4.5 wih bin and cat accuracies

--- ker_model_loader.py
+++ ker_model_loader.py
@@ -40,22 +40,30 @@ class KER_Model_Loader:
                 k.add(Dense(units=160, activation='relu'));
                 k.add(Dense(Model._D_Y_SHAPE[1], activation='sigmoid'));
                 k.compile(optimizer='adam', loss='mse', metrics=['accuracy'])
-                print("[KER_Model_loader: loaded build {0}.{1}]".format(Build, Make))
+                print("[KER_Model_loader: loaded new build {0}.{1}]".format(Build, Make))
             elif (Make == '2'):
                 k.add(LSTM(units=80, activation='relu', input_shape=(Model._D_X_SHAPE[1],Model._D_X_SHAPE[2]) ,  return_sequences=True ));
                 k.add(LSTM(units=160, activation='relu'))
                 k.add(Dense(units=160, activation='relu'));
                 k.add(Dense(Model._D_Y_SHAPE[1], activation='sigmoid'));
                 k.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy', 'binary_accuracy'])
-                print("[KER_Model_loader: loaded build {0}.{1}]".format(Build, Make))
+                print("[KER_Model_loader: loaded new build {0}.{1}]".format(Build, Make))
             elif (Make == '3'):
                 k.add(LSTM(units=80, activation='relu', input_shape=(Model._D_X_SHAPE[1],Model._D_X_SHAPE[2]) ,  return_sequences=True ));
                 k.add(LSTM(units=160, activation='relu'))
                 k.add(Dense(units=160, activation='relu'));
                 k.add(Dense(Model._D_Y_SHAPE[1], activation='sigmoid'));
                 k.compile(optimizer=Adam(learning_rate=0.01), loss='binary_crossentropy', metrics=['accuracy', 'binary_accuracy'])
-                #when u increase batchsize, decrease learning rate, so it learn slowly, take small step 
-                print("[KER_Model_loader: loaded build {0}.{1}]".format(Build, Make))
+                #when u increase batchsize, decrease learning rate, so it learn slowly, takes small step 
+                print("[KER_Model_loader: loaded new build {0}.{1}]".format(Build, Make))
+            elif (Make == '4'):
+                k.add(LSTM(units=80, activation='relu', input_shape=(Model._D_X_SHAPE[1],Model._D_X_SHAPE[2]) ,  return_sequences=True ));
+                k.add(LSTM(units=160, activation='relu'))
+                k.add(Dense(units=160, activation='relu'));
+                k.add(Dense(Model._D_Y_SHAPE[1], activation='sigmoid'));
+                k.compile(optimizer=Adam(learning_rate=0.01), loss='binary_crossentropy', metrics=['categorical_accuracy'])
+                #when u increase batchsize, decrease learning rate, so it learn slowly, takes small step 
+                print("[KER_Model_loader: loaded new build {0}.{1}]".format(Build, Make))
         
         Model._M = k;
     
